bdfff0ef44d23eedb63bbc68e04210e7cbb6e5f3
James-Coulson <78469310+James-Coulson@users.noreply.github.com>
2024-02-11 05:49:01 +0000

    feat: updated Device unit tests to include getAverageRainfallSince()

--- src/test/java/interfuze/DeviceTest.java
+++ src/test/java/interfuze/DeviceTest.java
@@ -15,25 +15,41 @@ import org.junit.Test;
 public class DeviceTest {
 	@Test
     public void testDevice() {
+        // Test data
         int deviceID = 1;
         String deviceName = "Device1";
         String location = "Location1";
 
+        // Create a device
         Device device = new Device(deviceID, deviceName, location);
 
+        // Test getters
         assertEquals(deviceID, device.getDeviceID());
         assertEquals(deviceName, device.getDeviceName());
         assertEquals(location, device.getLocation());
 
+        // Test toString
         String expectedToString = "Device [deviceID=" + deviceID + ", deviceName=" + deviceName + ", location=" + location + "]";
         assertEquals(expectedToString, device.toString());
 
+        // Test hashCode and equals
         Device sameDevice = new Device(deviceID, deviceName, location);
         assertEquals(device.hashCode(), sameDevice.hashCode());
         assertTrue(device.equals(sameDevice));
-
         Device differentDevice = new Device(2, deviceName, location);
         assertNotEquals(device.hashCode(), differentDevice.hashCode());
         assertFalse(device.equals(differentDevice));
+
+        // Test adding observations and calculating average rainfall
+        long currentTime = System.currentTimeMillis();
+        device.addObservation(new Observation(deviceID, currentTime - 1000, 10)); // 1 second ago
+        device.addObservation(new Observation(deviceID, currentTime - 2000, 20)); // 2 seconds ago
+        device.addObservation(new Observation(deviceID, currentTime - 3000, 30)); // 3 seconds ago
+
+        // The average rainfall since 2.5 seconds ago should be (10 + 20) / 2 = 15
+        assertEquals(15, device.getAverageRainfallSince(currentTime - 2500), 0.001);
+
+        // The average rainfall since 1.5 seconds ago should be 10
+        assertEquals(10, device.getAverageRainfallSince(currentTime - 1500), 0.001);
     }
 }
