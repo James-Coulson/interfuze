7caffee5330ff8b3cc08f8f8df4db3f998d33053
James-Coulson <78469310+James-Coulson@users.noreply.github.com>
2024-02-11 05:52:53 +0000

    feat: updated observations unit test to include toString

--- src/test/java/interfuze/ObservationTest.java
+++ src/test/java/interfuze/ObservationTest.java
@@ -15,22 +15,29 @@ import org.junit.Test;
 public class ObservationTest {
 	@Test
     public void testObservation() {
+        // Test data
         int deviceID = 1;
         long observationTime = 1627552800000L; // Thursday, 29 July 2021 10:00:00 GMT
         int rainfall = 10;
 
+        // Create an observation
         Observation observation = new Observation(deviceID, observationTime, rainfall);
 
+        // Test getters
         assertEquals(deviceID, observation.getDeviceID());
         assertEquals(observationTime, observation.getObservationTime());
         assertEquals(rainfall, observation.getRainfall());
 
+        // Test hashCode and equals
         Observation sameObservation = new Observation(deviceID, observationTime, rainfall);
         assertEquals(observation.hashCode(), sameObservation.hashCode());
         assertTrue(observation.equals(sameObservation));
-
         Observation differentObservation = new Observation(2, observationTime, rainfall);
         assertNotEquals(observation.hashCode(), differentObservation.hashCode());
         assertFalse(observation.equals(differentObservation));
+
+        // Test toString
+        String expectedToString = "Observations [deviceID=" + deviceID + ", observationTime=" + observationTime + ", rainfall=" + rainfall + "]";
+        assertEquals(expectedToString, observation.toString());
     }
 }
